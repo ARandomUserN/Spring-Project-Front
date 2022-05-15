a18ff43ad01d6410a2e0657486e5848e442db562
ARandomUserN <aforusinski@gmai.com>
2022-05-15 14:28:23 +0200

    Login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  
+  const [username, setName] = useState("");
+  const [password, setEmail] = useState("");
+  
+  let next;
+
+  let handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(username,password)
+    try {
+      let res = await axios({
+        method: "put",
+        url: "/login",
+        data: JSON.stringify({
+          username: username,
+          password: password
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        }
+
+      }).then(res => {
+        console.log(res.data)
+        let next = res.data["href"]
+        console.log(next)
+        // this.next = next
+        
+      })
+      
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={username}
+          placeholder="Email"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          value={password}
+          placeholder="Pwd"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <button type="submit">Create</button>
+      </form>
     </div>
   );
 }
